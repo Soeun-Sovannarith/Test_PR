fc3070535464aba0b5510b898b1735ae84f584ae
Soeun Sovannarith <seounsovanarith@gmail.com>
2025-10-31 11:14:34 +0700

    test: remove test cases
    Updated test coverage for test.py
    
    - 3 additions, 5 deletions

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def lcs(X, Y):
     for i in range(1, m+1):
         for j in range(1, n+1):
             if X[i-1] == Y[j-1]:
-                dp[i][j] = 1 + dp[i-1][j-1]
+                dp[i][j] = dp[i-1][j-1] + 1
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     return dp[m][n]
@@ -15,7 +15,5 @@ X = "ABCDEFG"
 Y = "HIJKLMN"
 print(f"LCS of '{X}' and '{Y}': {lcs(X, Y)}")
 
-
-print("Testing Pull Request")
-print("Another Testing of Pull Request")
-print("Longest Common Subsequence (LCS) Algorithm")
+print("testing")
+print("This is pull request testing")
